07022b09bca007db565f0e16e51aa1b0dc76b8c1
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-08-17 21:04:30 +0300

    Comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return_evens = [num for num in num_list if num % 2 == 0]
+    return return_evens
+
 
 def make_exclamation(sentence_list):
-    pass
+    make_exclamation = [string + "!" for string in sentence_list]
+    return make_exclamation
